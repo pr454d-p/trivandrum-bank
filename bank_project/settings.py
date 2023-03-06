1d578702e528561e8371e0c5cf7f14eaf8ee2a87
pr454d-p <7697prasad@gmail.com>
2023-03-06 11:28:38 +0530

    static root

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATICFILES_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
